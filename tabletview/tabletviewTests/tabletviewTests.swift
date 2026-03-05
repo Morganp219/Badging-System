3faa77bdc75a9cdfb6d1a89f0225fcbd731220d0
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-03-05 08:32:54 -0700

    chore: added backend & iOS TabletView

---
+++ tabletview/tabletviewTests/tabletviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  tabletviewTests.swift
+//  tabletviewTests
+//
+//  Created by Teaching on 3/5/26.
+//
+
+import Testing
+@testable import tabletview
+
+struct tabletviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
