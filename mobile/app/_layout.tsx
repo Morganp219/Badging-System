279da4d1eeb11df5670d2e7359e3fc77aabe8559
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-03-05 10:18:43 -0700

    feat: added auth to our mobile apps

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import React from "react";
+
+import { tokenCache } from '@clerk/expo/token-cache'
+import {ClerkProvider} from "@clerk/expo"
+import { ThemeProvider } from "@joe111/neo-ui";
+
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider>
+        <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
+    </ThemeProvider>
+  );
 }
