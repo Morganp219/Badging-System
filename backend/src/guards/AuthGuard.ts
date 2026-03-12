76c916aa16d0ddec08b14e3facd15abb289f59ac
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-03-12 08:39:18 -0600

    feat: added auth

---
+++ backend/src/guards/AuthGuard.ts
@@ -0,0 +1,23 @@
+import { verifyToken } from "@clerk/backend";
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
+import "dotenv/config"
+export class AuthGuard implements CanActivate {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest()
+        const authHeader = request.headers.authorization;
+        if(!authHeader) {
+            return false
+        }
+        // Bearer <Token Goes Here>
+        const token = authHeader.replace("Bearer ", "")
+        const payload = await verifyToken(token, {
+            secretKey: process.env.CLERK_SECRET_KEY
+        })
+        if(payload) {
+            request.user = payload
+            return true
+        }
+        return false
+    }
+}
